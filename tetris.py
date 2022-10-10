2ee4b16cc2152683b718c4c35e95b4031815625e
Robbie Clark <robbietclark@gmail.com>
2022-10-10 11:11:04 +0100

    Removed all the background etc from the window

--- tetris.py
+++ tetris.py
@@ -21,11 +21,17 @@ win = Tk()
 win.geometry("480x640")
 win.resizable(width=0, height=0)
 win.title("Tetris")
-#win.overrideredirect(1)
+win.overrideredirect(1)
+win.config(highlightbackground = '#000000')
+win.wm_attributes('-transparentcolor','#000000')
+win.wm_attributes('-topmost', True)
 
 # Define a Canvas widget
-canvas = Canvas(win, width=480, height=640, bg="black", highlightthickness=0)
+canvas = Canvas(win, width=480, height=640, background = "#000000", highlightthickness=0, borderwidth = 0)
 canvas.pack()
+
+label = Label(canvas, borderwidth=0,bg='#000000')
+canvas.create_window(0, 0, anchor=NW, window=label)
 
 # Add Images to Canvas widget
 image_0 = ImageTk.PhotoImage(Image.open('board_png/tetris_board_blank.png'))
@@ -33,6 +39,23 @@ img_0 = canvas.create_image(0, 0, anchor=NW, image=image_0) #-20,-20
 
 image_1 = ImageTk.PhotoImage(Image.open('blocks_png/BlueBlock150.png'))
 img_1 = canvas.create_image(21, 18, anchor=NW, image=image_1)
+
+txt_0 = canvas.create_text(100,100, text = "HELLO WORLD", fill = "white", font = ("Courier 15 bold"))
+
+offsetx = 0
+offsety = 0
+
+def drag(event):
+    x = win.winfo_pointerx() - win.offsetx
+    y = win.winfo_pointery() - win.offsety
+    win.geometry('+{x}+{y}'.format(x=x,y=y))
+
+def click(event):
+    win.offsetx = event.x
+    win.offsety = event.y 
+
+win.bind('<Button-1>', click)
+win.bind('<B1-Motion>', drag)
 
 # x = 21
 # y = 18
@@ -43,8 +66,6 @@ img_1 = canvas.create_image(21, 18, anchor=NW, image=image_1)
 #    for i in range(10):
 #       ls.append(ImageTk.PhotoImage(Image.open('blocks_jpg/BlueBlock150.jpg')))
 #       ls_1.append(canvas.create_image(x + (i * 30), y  + (30 * j), anchor=NW, image=ls[i]))
-
-
 
 def left(e):
    x = -30
